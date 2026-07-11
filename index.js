ddf26e62749c76d0b7a6d9e320c54364958a9e0f
Tiago Pereira <tiagonet50@gmail.com>
2026-07-11 17:11:21 -0300

    index.js

--- index.js
+++ index.js
@@ -304,8 +304,10 @@ async function startSpoofedSession() {
     })
 }
 
-if (process.env.CLEAN_DOWNLOADS === "true")
-
+if (process.env.CLEAN_DOWNLOADS === "true") {
+    console.log(`Downloads cleanup is enabled. Cleaning every ${CLEANUP_HOURS} hours.`)
+    startDownloadsCleanup(cleanFilenDownloads, DOWNLOADS_CLEANUP_INTERVAL)
+}
 setInterval(async () => {
     await cleanFilenDownloads()
 }, DOWNLOADS_CLEANUP_INTERVAL)
