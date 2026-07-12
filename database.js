4775e2a62447cef0a96e06d1ef18c6c4d6f44760
Tiago Pereira <tiagonet50@gmail.com>
2026-07-11 21:48:46 -0300

    database.js

--- database.js
+++ database.js
@@ -2,11 +2,9 @@ import { Pool } from "pg";
 
 export const db = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: process.env.DATABASE_CA ? {
-        rejectUnauthorized: false,
-        ca: process.env.DATABASE_CA,
-    } : {
-        rejectUnauthorized: false
+    ssl: {
+        ca: process.env.DATABASE_CA.replace(/\\n/g, "\n"),
+        rejectUnauthorized: true
     }
 });
 
